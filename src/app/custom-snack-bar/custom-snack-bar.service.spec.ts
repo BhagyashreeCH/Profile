f230172ac698a7b03d144bfb11d1d0ef40b56146
Karamadi <basalingyya.channayya@siemens-healthineers.com>
2020-07-02 10:00:19 +0530

    test

--- src/app/custom-snack-bar/custom-snack-bar.service.spec.ts
+++ src/app/custom-snack-bar/custom-snack-bar.service.spec.ts
@@ -1,16 +1,64 @@
 import { TestBed } from '@angular/core/testing';
 
 import { CustomSnackBarService } from './custom-snack-bar.service';
+import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
+import { of } from 'rxjs';
 
-describe('CustomSnackBarService', () => {
+fdescribe('CustomSnackBarService', () => {
   let service: CustomSnackBarService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [
+        BrowserDynamicTestingModule,
+        HttpClientTestingModule,
+        MatSnackBarModule,
+      ],
+      providers: [{ provide: MatSnackBar, useValue: MatSnackBarMock }],
+      schemas: [NO_ERRORS_SCHEMA],
+    });
     service = TestBed.inject(CustomSnackBarService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  it('should call openSnackBarInQueue', () => {
+    const res = [
+      { type: 'data', msg: 'test' },
+      { type: 'data', msg: 'test' },
+    ];
+    const cbObj = {
+      cbFun: (response) => {},
+    };
+    spyOn(cbObj, 'cbFun');
+    service.openSnackBarInQueue('test', cbObj.cbFun, res);
+    expect(cbObj.cbFun).toHaveBeenCalledWith(res);
+  });
+
+  it('should call openSnackBarInQueue with 0 call back', () => {
+    const res = [];
+    const cbObj = {
+      cbFun: (response) => {},
+    };
+    spyOn(cbObj, 'cbFun');
+    service.openSnackBarInQueue('test', cbObj.cbFun, res);
+    expect(cbObj.cbFun).toHaveBeenCalledTimes(0);
+  });
 });
+
+const MatSnackBarMock = {
+  openFromComponent: () => {
+    return {
+      instance: {
+        texttodisplay: '',
+        isSuccess: '',
+      },
+      afterDismissed: () => of(true),
+    };
+  },
+};
